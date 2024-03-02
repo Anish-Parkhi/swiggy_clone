192477a95bafb80e7172007e0c9045e2f8b316e5
Anish-Parkhi <anishparkhi03@gmail.com>
2024-03-02 11:01:09 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import './index.css';
+
+function App() {
+  return (
+    <>
+      <div className="text-3xl font-bold underline">Jai Babaji</div>
+    </>
+  );
+}
+
+export default App;
