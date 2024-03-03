6e69dac04ec2c69d9d1b885fc4db5ac6d86e27b9
Anish-Parkhi <anishparkhi03@gmail.com>
2024-03-03 14:13:03 +0530

    feat: added cart component

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,13 +1,41 @@
 import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 import CartContext from '../../utils/CartContext';
+import CartItems from './CartItems';
 
 const Cart = () => {
   const cartData = useContext(CartContext);
-  console.log(cartData.cartItems);
+  const navigate = useNavigate();
+
+  const EmptyCart = () => {
+    return (
+      <div className="flex flex-col justify-between items-center gap-8 p-2">
+        <div className="text-xl">Your cart is empty</div>
+        <div>You can go to home page to view more restaurants</div>
+        <button
+          onClick={() => navigate('/')}
+          className="border-2 p-2 bg-orange-700	text-white rounded"
+        >
+          SEE RESTAURANTS NEAR YOU
+        </button>
+      </div>
+    );
+  };
 
   return (
-    <div>
-      <div>Cart</div>
+    <div className="w-2/3 m-auto p-3 ">
+      <div>
+        {cartData.length > 0 ? (
+          <>
+            <div className="font-bold text-lg	">Cart</div>
+            cartData?.cartItems?.map((item, index) => (
+            <CartItems data={item} key={index} />
+            ))
+          </>
+        ) : (
+          <EmptyCart />
+        )}
+      </div>
     </div>
   );
 };
---
+++ src/components/CartItems.jsx
@@ -0,0 +1,16 @@
+import CDNURL from '../../utils/common/cdnURl';
+
+const CartItems = ({ data }) => {
+  console.log(data);
+  return (
+    <div className="flex justify-between items-center border-b-2 p-2">
+      <div>
+        <div className="font-bold text-lg">{data?.name}</div>
+        <div>₹ {data?.price / 100}</div>
+      </div>
+      <img className="w-1/6" src={CDNURL + data?.imageId} />
+    </div>
+  );
+};
+
+export default CartItems;
